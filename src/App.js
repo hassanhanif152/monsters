da5306c688a3461928992859d71f171b19bedfab
PC <hassanhanif152@gmail.com>
2020-09-20 10:32:40 +0500

    Initial Stuff

---
+++ src/App.js
@@ -0,0 +1,67 @@
+// import React, { Component } from "react";
+// import "./App.css";
+// import { CardList } from "./components/card-list/card-list.component";
+// import { SearchBox } from "./components/search-box/search-box.component";
+
+// class App extends Component {
+// 	constructor() {
+// 		super();
+// 		this.state = {
+// 			monsters: [],
+// 			searchField: "",
+// 		};
+// 	}
+
+// 	componentDidMount() {
+// 		fetch("https://jsonplaceholder.typicode.com/users")
+// 			.then((response) => response.json())
+// 			.then((users) => this.setState({ monsters: users }));
+// 	}
+
+// 	render() {
+// 		const { monsters, searchField } = this.state;
+// 		const filteredMonsters = monsters.filter((monster) =>
+// 			monster.name.toLowerCase().includes(searchField.toLowerCase())
+// 		);
+
+// 		return (
+// 			<div className='App'>
+// 				<SearchBox
+// 					placeholder='search monsters'
+// 					handleChange={(e) => this.setState({ searchField: e.target.value })}
+// 				/>
+// 				<CardList monsters={filteredMonsters}></CardList>
+// 			</div>
+// 		);
+// 	}
+// }
+// export default App;
+
+import React, { Component } from "react";
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			monsters: [],
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) => this.setState({ monsters: users }));
+	}
+
+	render() {
+		return (
+			<div>
+				{this.state.monsters.map((monster) => (
+					<h1>{monster.name}</h1>
+				))}
+			</div>
+		);
+	}
+}
+
+export default App;
